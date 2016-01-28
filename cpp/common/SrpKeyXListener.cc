675326e6c07887f1b6ac88824c4b2a514c079408
Jorge Martinez Hernandez <jmartinez@affinegy.com>
2016-01-28 12:57:48 -0600

    ASAGW-74 Remove dependency on samples_common
    Change-Id: I65a902e61e94e94d7bb6dcc2c8dc1b8fe33425b0
    Signed-off-by: Jorge Martinez Hernandez <jmartinez@affinegy.com>

--- cpp/common/SrpKeyXListener.cc
+++ cpp/common/SrpKeyXListener.cc
@@ -15,12 +15,13 @@
  ******************************************************************************/
 
 #include "SrpKeyXListener.h"
-#include "../GatewayConstants.h"
+#include <qcc/Debug.h>
 
 #define DEFAULT_PASSCODE "000000"
 
-using namespace ajn;
-using namespace gw;
+namespace ajn {
+namespace gw {
+namespace common {
 
 SrpKeyXListener::SrpKeyXListener() : m_PassCode(DEFAULT_PASSCODE), m_GetPassCode(0)
 {
@@ -48,7 +49,6 @@ bool SrpKeyXListener::RequestCredentials(const char* authMechanism, const char* 
 {
     QCC_UNUSED(userId);
     QCC_DbgPrintf(("RequestCredentials for authenticating %s using mechanism %s", authPeer, authMechanism));
-
     if (strcmp(authMechanism, "ALLJOYN_SRP_KEYX") == 0 || strcmp(authMechanism, "ALLJOYN_ECDHE_PSK") == 0) {
         if (credMask & AuthListener::CRED_PASSWORD) {
             if (authCount <= 3) {
@@ -56,7 +56,6 @@ bool SrpKeyXListener::RequestCredentials(const char* authMechanism, const char* 
                 if (m_GetPassCode) {
                     m_GetPassCode(passCodeFromGet);
                 }
-                QCC_DbgPrintf(("RequestCredentials setPasscode to %s", m_PassCode.c_str()));
                 creds.SetPassword(m_GetPassCode ? passCodeFromGet.c_str() : m_PassCode.c_str());
                 return true;
             } else {
@@ -71,4 +70,8 @@ void SrpKeyXListener::AuthenticationComplete(const char* authMechanism, const ch
 {
     QCC_UNUSED(authPeer);
     QCC_DbgPrintf(("Authentication with %s %s", authMechanism, (success ? " was successful" : " failed")));
+}
+
+}
+}
 }
