3442141538cfadbf60bfe5afbbfc66589b15c126
George Tang <gtang@affinegy.com>
2016-02-08 21:54:05 -0600

    ASAGW-56 ConfigClient in JavaGWController
    Implemented configclient in java gateway controller. The config client
    supports setting password and factory reset of the gateway agent.
    
    Change-Id: I1485ecdc6b8f79162e026945843e1a357328d0aa
    Signed-off-by: George Tang <gtang@affinegy.com>

--- java/GatewayController/src/org/alljoyn/gatewaycontroller/GWControllerSampleApplication.java
+++ java/GatewayController/src/org/alljoyn/gatewaycontroller/GWControllerSampleApplication.java
@@ -20,6 +20,7 @@ import org.alljoyn.bus.BusAttachment;
 import org.alljoyn.bus.SessionOpts;
 import org.alljoyn.bus.Status;
 import org.alljoyn.bus.alljoyn.DaemonInit;
+import org.alljoyn.config.ConfigServiceImpl;
 import org.alljoyn.gatewaycontroller.sdk.Acl;
 import org.alljoyn.gatewaycontroller.sdk.AclRules;
 import org.alljoyn.gatewaycontroller.sdk.ConnectorApp;
@@ -29,6 +30,9 @@ import org.alljoyn.gatewaycontroller.sdk.GatewayMgmtApp;
 import org.alljoyn.gatewaycontroller.sdk.GatewayMgmtAppListener;
 import org.alljoyn.gatewaycontroller.sdk.RemotedApp;
 import org.alljoyn.gatewaycontroller.sdk.ajcommunication.CommunicationUtil.SessionResult;
+import org.alljoyn.services.android.security.AuthPasswordHandler;
+import org.alljoyn.services.android.security.SrpAnonymousKeyListener;
+import org.alljoyn.services.android.utils.AndroidLogger;
 import org.alljoyn.gatewaycontroller.sdk.ajcommunication.GatewayControllerSessionListener;
 
 import android.app.Application;
@@ -71,7 +75,7 @@ public class GWControllerSampleApplication extends Application implements Gatewa
             super.sessionJoined(result);
 
             GWControllerSampleApplication app = GWControllerSampleApplication.this;
-            Status status                     = result.getStatus();
+            Status status = result.getStatus();
 
             if (status == Status.OK || status == Status.ALLJOYN_JOINSESSION_REPLY_ALREADY_JOINED) {
 
@@ -91,7 +95,7 @@ public class GWControllerSampleApplication extends Application implements Gatewa
      * The daemon should advertise itself "quietly" (directly to the calling
      * port) This is to reply directly to a TC looking for a daemon
      */
-    private static final String DAEMON_NAME_PREFIX  = "org.alljoyn.BusNode.IoeService";
+    private static final String DAEMON_NAME_PREFIX = "org.alljoyn.BusNode.IoeService";
 
     /**
      * The daemon should advertise itself "quietly" (directly to the calling
@@ -175,8 +179,19 @@ public class GWControllerSampleApplication extends Application implements Gatewa
      *
      * @param passcode
      */
-    public void setGatewayPasscode(String passcode) {
-        authManager.setPassCode(passcode);
+    public void setGatewayPasscode(String passcode, String busName, String appId) {
+        bus.enableConcurrentCallbacks();
+        authManager.setPassCode(passcode, busName, appId);
+    }
+
+    /**
+     * Provide the {@link AuthManager} with gateway factoryReset
+     *
+     * @param passcode
+     */
+    public void doGatewayFactoryReset(String busName) {
+        bus.enableConcurrentCallbacks();
+        authManager.doFactoryReset(busName);
     }
 
     /**
@@ -296,7 +311,7 @@ public class GWControllerSampleApplication extends Application implements Gatewa
 
             Log.v(TAG, "App: '" + remApp + "', ObjDesc: '" + remApp.getRuleObjectDescriptions() + "'");
             Log.v(TAG, "------------------------------------------------");
-        }// for :: remotedApp
+        } // for :: remotedApp
 
         Log.d(TAG, "========================= ");
     }
@@ -358,13 +373,15 @@ public class GWControllerSampleApplication extends Application implements Gatewa
 
                 bus.releaseName(daemonName);
                 Log.e(TAG, "Failed to advertise daemon name " + daemonName + ", Error: '" + adStatus + "'");
-                throw new GatewayControllerException("Failed to advertise daemon name '" + daemonName + " ', Error: '" + adStatus + "'");
+                throw new GatewayControllerException(
+                        "Failed to advertise daemon name '" + daemonName + " ', Error: '" + adStatus + "'");
             } else {
                 Log.d(TAG, "Succefully advertised daemon name: '" + daemonName + "'");
             }
         } else {
             Log.d(TAG, "Failed to request the daemon name: '" + daemonName + "', Error: '" + reqStatus + "' ");
-            throw new GatewayControllerException("Failed to request the DaemonName: '" + daemonName + "',  Error: '" + reqStatus + "'");
+            throw new GatewayControllerException(
+                    "Failed to request the DaemonName: '" + daemonName + "',  Error: '" + reqStatus + "'");
         }
 
     }// advertiseDaemon
